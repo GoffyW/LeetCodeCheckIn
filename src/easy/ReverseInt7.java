413e16f3b536f27a4b634757a421f98957f5676b
guofei <13161198100@163.com>
2020-04-21 19:18:16 +0800

    修改逻辑BUG

--- src/easy/ReverseInt7.java
+++ src/easy/ReverseInt7.java
@@ -17,7 +17,7 @@ public class ReverseInt7 {
             x /= 10;
             if (rev>Integer.MAX_VALUE/10||(rev == Integer.MAX_VALUE/10&&pop>7))
                 return 0;
-            if (rev<Integer.MIN_VALUE/10||(rev == Integer.MIN_VALUE/10&&pop>-8))
+            if (rev<Integer.MIN_VALUE/10||(rev == Integer.MIN_VALUE/10&&pop<-8))
                 return 0;
             rev = rev * 10 + pop;
         }
