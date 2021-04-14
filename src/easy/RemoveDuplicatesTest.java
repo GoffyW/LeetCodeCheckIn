7405d557688d3ba2376a8cef45d0167ad52f1e2d
Guo Goffy <guogoffy@Goffys-MacBook.local>
2021-04-14 22:07:49 +0800

    删除有序数组中的重复项练习

---
+++ src/easy/RemoveDuplicatesTest.java
@@ -0,0 +1,25 @@
+package easy;
+
+/**
+ * @author guogoffy
+ */
+public class RemoveDuplicatesTest {
+    public static void main(String[] args) {
+        int nums [] = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(removeDuplicates(nums));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        int p = 0;
+        int q = 1;
+        while(q < nums.length){
+            if(nums[p] != nums[q]){
+                nums[p + 1] = nums[q];
+                p++;
+            }
+            q++;
+        }
+        return p + 1;
+    }
+}
